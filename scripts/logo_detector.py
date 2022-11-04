863bcbf3bf27121024ae1538e1c9e942c3dbfe30
Janerose <janerosenyams@gmail.com>
2022-11-04 16:01:42 +0000

    modifies face detection files

--- scripts/logo_detector.py
+++ scripts/logo_detector.py
@@ -1,18 +1,25 @@
 import cv2
+import numpy as np
+import os
+import pickle as pkl
+
+
 class LogoDetector:
     def __init__(self) -> None:
         pass
-    
+
     def createDetector(self):
         detector = cv2.ORB_create(nfeatures=2000)
         return detector
-    def getFeatures(self,img):
+
+    def getFeatures(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        detector = createDetector()
+        detector = self.createDetector()
         kps, descs = detector.detectAndCompute(gray, None)
         return kps, descs, img.shape[:2][::-1]
-    
-    def detectFeatures(self, img, train_features):
+
+    def detectFeatures(self, imagePath, train_features):
+        img = cv2.imread(imagePath)
         train_kps, train_descs, shape = train_features
         # get features from input image
         kps, descs, _ = self.getFeatures(img)
@@ -35,29 +42,56 @@ class LogoDetector:
             return None
         # estimate a transformation matrix which maps keypoints from train image coordinates to sample image
         src_pts = np.float32([train_kps[m[0].queryIdx].pt for m in good
-                            ]).reshape(-1, 1, 2)
+                              ]).reshape(-1, 1, 2)
         dst_pts = np.float32([kps[m[0].trainIdx].pt for m in good
-                            ]).reshape(-1, 1, 2)
+                              ]).reshape(-1, 1, 2)
+
         m, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-        if m is not None:
-            # apply perspective transform to train image corners to get a bounding box coordinates on a sample image
-            scene_points = cv2.perspectiveTransform(np.float32([(0, 0), (0, shape[0] - 1), (shape[1] - 1, shape[0] - 1), (shape[1] - 1, 0)]).reshape(-1, 1, 2), m)
-            rect = cv2.minAreaRect(scene_points)
-            # check resulting rect ratio knowing we have almost square train image
-            if rect[1][1] > 0 and 0.8 < (rect[1][0] / rect[1][1]) < 1.2:
-                return rect
-        return None
-    
-features = LogoDetector()
+        return m, mask
+"""
+if m is not None:
+# apply perspective transform to train image corners to get a bounding box coordinates on a sample image
+scene_points = cv2.perspectiveTransform(np.float32(
+[(0, 0), (0, shape[0] - 1), (shape[1] - 1, shape[0] - 1), (shape[1] - 1, 0)]).reshape(-1, 1, 2), m)
+rect = cv2.minAreaRect(scene_points)
+# check resulting rect ratio knowing we have almost square train image
+if rect[1][1] > 0 and 0.4 < (rect[1][0] / rect[1][1]) < 1.2:
+# return bounding box
+return rect
+return None
+"""
+
+
+if __name__ == "__main__":
+    dir_path = os.getcwd()
+
+    assets_path = dir_path + "/Assets"
+
+    # Change Asset folder name
+    fol_name = '/2b7e702f208b7fd60d15d0bdadd269f4'
+
+    # Change File name
+    file_name = '/_preview.png'
+
+    image_path = assets_path + fol_name + file_name
+
+    features = LogoDetector()
     # get train features
-img = cv2.imread('logo_train.png')
-train_features = features.getFeatures(img)
-# detect features on test image
-region = features.detectFeatures(frame, train_features)
+    img = cv2.imread(image_path)
+    train_features = features.getFeatures(img)
+    # detect features on test image
+    retval, mask = features.detectFeatures(image_path, train_features)
+    
+"""
 if region is not None:
-    # draw rotated bounding box
-    box = cv2.boxPoints(region)
-    box = np.int0(box)
-    cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
-# display the image
+Saving the bounding box coordinates in a pickle file
+with open('_preview.pkl', 'wb') as f:
+pkl.dump(region, f)
+draw rotated bounding box
+box = cv2.boxPoints(region)
+box = np.int0(box)
+cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+    
+display the image
 cv2.imshow("Preview", img)
+"""
